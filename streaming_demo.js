048149eab548403232f3e51a07a6fc9a0cd5e37d
zlee1 <zack.lee@valpo.edu>
2022-03-03 14:05:59 -0600

    reads json data and outputs in console

--- streaming_demo.js
+++ streaming_demo.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+service_genres_counted_url = "https://raw.githubusercontent.com/zlee1/StreamingServiceAnalysis/master/data/modified/service_genres_counted.txt"
+var genres = null;
+
+fetch(service_genres_counted_url)
+    .then(response => response.text())
+    .then(text => {
+        genres = JSON.parse(text);
+        console.log(genres);
+    });
+
+function get_service_genres(service){
+  console.log(genres);
+  for(var i = 0; i < genres.service.length; i++){
+    if(genres.service[i] == service){
+      console.log(genres.genre[i] + ": " + genres.percentage_of_total[i]);
+    }
+  }
+}
+
+document.getElementById("get_genres").addEventListener('click', () =>{
+  get_service_genres("hbo");
+});
