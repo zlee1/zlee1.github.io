b8b9d1588bf613385d0e277faf09df5c640cb922
zlee1 <zack.lee@valpo.edu>
2022-03-22 12:47:18 -0500

    no longer in demo, but still work in progress

--- streaming.js
+++ streaming.js
@@ -448,9 +448,9 @@ function update_plot(set, key){
 
 document.getElementById("back").addEventListener('click', () =>{
   if(document.getElementById("sect_head").innerHTML == ""){
-    location.href = "ss_recommend.html";
+    location.href = "index.html";
   }else if(document.getElementById("sect_head").innerHTML == order[0].toUpperCase() + "S"){
-    location.href = "streaming_demo.html";
+    location.href = "ss_recommend.html";
   }else if(document.getElementById("sect_head").innerHTML == order[1].toUpperCase() + "S"){
     add_inputs(order[0]);
   }else if(document.getElementById("sect_head").innerHTML == order[2].toUpperCase() + "S"){
